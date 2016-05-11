27bf75c70d314e9675381b3944f3ac0bc2ab45f4
ElenaErmoshina <lena@pavlovic.ru>
2016-05-11 10:47:52 -0700

    Added GreenSock styles

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+TweenLite.to(".smallimage", 5, { ease: SlowMo.ease.config(0.7, 0.4, false), y: 100, x: 100 }); 
+});
---
+++ js/site.js
@@ -0,0 +1,14 @@
+	$(function(){
+		var mouseIn = function(){
+			$(this).find("a").addClass("animated pulse");
+			//console.debug($(this).find("a"));
+			$("#h1").addClass("animated pulse");
+			$("#l2").addClass("animated pulse");
+		};
+		var mouseOut = function(){
+			$(this).find("a").removeClass("animated pulse");
+			$("#h1").removeClass("animated pulse");
+			$("#l2").removeClass("animated pulse");
+		};
+		$(".menulist > li").hover(mouseIn, mouseOut);
+	});
