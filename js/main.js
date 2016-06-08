dde3eb0f0970e267c44d7a5532d9ae535b09a490
ElenaErmoshina <lena@pavlovic.ru>
2016-06-07 23:46:51 -0700

    Updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-TweenLite.to(".smallimage", 5, { ease: SlowMo.ease.config(0.7, 0.4, false), y: 100, x: 100 }); 
+TweenLite.to(".smallimage", 5, { ease: SlowMo.ease.config(0.5, 0.2, false), y: 100, x: 100 }); 
 });
--- js/site.js
+++ js/site.js
@@ -1,14 +1,25 @@
 	$(function(){
-		var mouseIn = function(){
-			$(this).find("a").addClass("animated pulse");
+		//var mouseIn = function(){
+		//	$(this).find("a").addClass("animated pulse");
 			//console.debug($(this).find("a"));
-			$("#h1").addClass("animated pulse");
-			$("#l2").addClass("animated pulse");
-		};
-		var mouseOut = function(){
-			$(this).find("a").removeClass("animated pulse");
-			$("#h1").removeClass("animated pulse");
-			$("#l2").removeClass("animated pulse");
-		};
-		$(".menulist > li").hover(mouseIn, mouseOut);
-	});
+		//	$("#h1").addClass("animated pulse");
+		//	$("#l2").addClass("animated pulse");
+		//};
+		//var mouseOut = function(){
+		//	$(this).find("a").removeClass("animated pulse");
+		//	$("#h1").removeClass("animated pulse");
+		//	$("#l2").removeClass("animated pulse");
+		//};
+		//$(".menulist > li").hover(mouseIn, mouseOut);
+	
+	
+	
+		$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		items:1,
+		   });
+	
+});	
+	
